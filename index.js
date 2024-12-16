3921fae3838933583429c59152878e1e9ecb676d
Sriya77 <sriyachakinala.com>
2024-12-16 13:53:00 +0530

    finalversion06

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const app = express();
-const cors = require('cors')
-let leet = require('./leetcodee');
+const express = require('express');
+const cors = require('cors');
 const path = require('path');
+// const fetch = require('node-fetch'); // Ensure this package is installed
+
+const app = express();
+app.use(cors({ origin: '*' }));
 app.use(express.static('public'));
 
-app.use(cors({
-    origin: '*'
-}))
-console.log("hello");
-
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/:id', leet.leetcode);
+
+app.get('/:id', (req, res) => {
+  const user = req.params.id;
+  const backendURL = `http://localhost:4000/leetcode/${user}`; // Update URL if deployed
+
+  fetch(backendURL)
+    .then((response) => response.json())
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error('Error fetching from backend:', err);
+      res.status(500).send(err);
+    });
+});
 
 app.listen(3000, () => {
-    console.log(`App is running on port 3000`);
+  console.log('Leetics frontend is running on port 3000');
 });
