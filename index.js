a70f3f229cb7ff7dfef6ac7811a1b075d124e832
Sriya77 <sriyachakinala.com>
2024-12-16 13:21:35 +0530

    finalversion04

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-let leet = require('./leetcode');
+let leet = require('./leetcodee');
 const path = require('path');
 app.use(express.static('public'));
 
