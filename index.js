e75c41c61aa1d8bb1cfabbcc12f6caa7fb400959
Sriya77 <sriyachakinala.com>
2025-01-20 23:39:02 +0530

    finalversion08

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-// const express = require('express');
-// const cors = require('cors');
-// const path = require('path');
-// const fetch = require('node-fetch'); // Ensure this package is installed
-
-// const app = express();
-// app.use(cors({ origin: '*' }));
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// app.get('/:id', (req, res) => {
-//   const user = req.params.id;
-//   const backendURL = `https://leetics-backend.vercel.app/${user}`; // Update URL if deployed
-
-//   fetch(backendURL)
-//     .then((response) => response.json())
-//     .then((data) => res.send(data))
-//     .catch((err) => {
-//       console.error('Error fetching from backend:', err);
-//       res.status(500).send(err);
-//     });
-// });
-
-// app.listen(3000, () => {
-//   console.log('Leetics frontend is running on port 3000');
-// });
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors')
-let leet = require('./leetcodee');
-const path = require('path');
-app.use(express.static('public'));
+const cors = require("cors");
+let leet = require("./leetcodee");
+const path = require("path");
+const { rateLimit } = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+});
+
+const specificLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  message: "<b>Too many requests for this route, please try again later.</b>",
+});
+
+app.use(limiter);
+
+app.use(express.static("public"));
 
 app.use(cors({
-    origin: '*'
-}))
-console.log("hello");
+    origin: '*',
+    methods: ['GET', 'POST'],
+}));
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-app.get('/:id', leet.leetcode);
+app.get("/:id",specificLimiter, leet.leetcode);
 
 app.listen(3000, () => {
-    console.log(`App is running on port 3000`);
+  console.log(`App is running on port 3000`);
 });
